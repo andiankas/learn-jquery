aeeb132b23190c1c3c06fe02d401f6f5bfad7d72
andiankas <rustiantoandi17@gmail.com>
2018-05-02 20:22:35 -0700

    event

--- assets/jquery/script.js
+++ assets/jquery/script.js
@@ -2,9 +2,42 @@
 
 $(function(){
 
-	// alert("hello world");
-	$('button').click(function(){
-		$('#box').fadeOut(1000);
+	// // Simple syntax 01
+	// $('button').click(function(){
+	// 	$('#box').fadeOut(1000);
+	// });
+
+	// ----------------------------
+
+	// // Selector 02
+	// $('h2,h3,h4').css('border','solid 1px red');
+
+	// ----------------------------
+
+	// 03 EVENTS
+	$('#box').click(function() {
+		alert('you just clicked the box');
 	});
+
+	$('input').blur(function() {
+		if ($(this).val() == "" ) {
+			$(this).css('border','solid 1px red');
+			$('#box').text('forgot to add text?');
+		}
+	});
+
+	$('input').keydown(function() {
+		if ($(this).val() !== "") {
+			$(this).css('border','solid 1px #777');
+			$('#box').text('Thankyou!');
+		}
+	});
+
+	$('#box').hover(function() {
+		$(this).text('you hovered in!');
+	}, function(){
+		$(this).text('you hovered out!')
+	});
+
 
 });
