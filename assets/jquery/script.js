1b24ca9db77af74d6a1de9b7b72305e02fd583fa
andiankas <rustiantoandi17@gmail.com>
2018-05-02 20:49:15 -0700

    chaining

--- assets/jquery/script.js
+++ assets/jquery/script.js
@@ -14,30 +14,37 @@ $(function(){
 
 	// ----------------------------
 
-	// 03 EVENTS
-	$('#box').click(function() {
-		alert('you just clicked the box');
-	});
+	// // 03 EVENTS
+	// $('#box').click(function() {
+	// 	alert('you just clicked the box');
+	// });
 
-	$('input').blur(function() {
-		if ($(this).val() == "" ) {
-			$(this).css('border','solid 1px red');
-			$('#box').text('forgot to add text?');
-		}
-	});
+	// $('input').blur(function() {
+	// 	if ($(this).val() == "" ) {
+	// 		$(this).css('border','solid 1px red');
+	// 		$('#box').text('forgot to add text?');
+	// 	}
+	// });
 
-	$('input').keydown(function() {
-		if ($(this).val() !== "") {
-			$(this).css('border','solid 1px #777');
-			$('#box').text('Thankyou!');
-		}
-	});
+	// $('input').keydown(function() {
+	// 	if ($(this).val() !== "") {
+	// 		$(this).css('border','solid 1px #777');
+	// 		$('#box').text('Thankyou!');
+	// 	}
+	// });
 
-	$('#box').hover(function() {
-		$(this).text('you hovered in!');
-	}, function(){
-		$(this).text('you hovered out!')
-	});
+	// $('#box').hover(function() {
+	// 	$(this).text('you hovered in!');
+	// }, function(){
+	// 	$(this).text('you hovered out!')
+	// });
+	// // -----------------------------------------
 
+	// // 04 CHAINING
+	// $('.notification-bar').delay(2000).slideDown().delay(3000).fadeOut();
+	// //---------------------------------------
+
+	// 05 HIDING, SHOWING & FADING CONTENT
+	
 
 });
